314009265a987afb8238d818f7e34ed5eeae1d43
diegomarzaa <al426641@uji.es>
2024-01-25 12:51:01 +0100

    v1

---
+++ Sum/sum.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int number;
+    int sum = 0;
+
+    std::cout << "Enter a number: " << std::endl;
+    std::cin >> number;
+
+    std::cout << "The sum is " << sum << "!" << std::endl;
+    return 0;
+}
