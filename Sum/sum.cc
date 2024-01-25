79f63823dee96c44f0ffa324ed8789d2da8c2189
diegomarzaa <al426641@uji.es>
2024-01-25 13:00:36 +0100

    v3

--- Sum/sum.cc
+++ Sum/sum.cc
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
 
+int sumar(int n) {
+    int sum = 0;
+    for (int i = 1; i<=n; i++) {
+    sum += i;
+    }
+    return sum;
+}
+
 int main() {
     int number;
-    int sum = 0;
-
     std::cout << "Enter a number: " << std::endl;
     std::cin >> number;
 
-    for (int i = 1; i<=number; i++) {
-        sum += i;
-    }
-
-    std::cout << "The sum from 1 to " << number << " is " << sum << std::endl;
+    std::cout << "The sum from 1 to " << number << " is " << sumar(number) << std::endl;
     return 0;
 }
