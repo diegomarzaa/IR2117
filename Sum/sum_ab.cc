569dbc1e58ccded45c3c52fd5ed0b5fa4270b904
diegomarzaa <al426641@uji.es>
2024-01-25 13:30:40 +0100

    sum_ab v1

---
+++ Sum/sum_ab.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int a, b;
+    int sum = 0;
+    std::cout << "Enter a number >= 1: " << std::endl;
+    std::cin >> a;
+
+    std::cout << "Enter b number >= 1: " << std::endl;
+    std::cin >> b;
+
+    for (int num_act = a; num_act<=b; num_act++) {
+        sum += num_act;
+    }
+
+    std::cout << "The sum from " << a << " to " << b << " is " << sum << std::endl;
+    return 0;
+}
