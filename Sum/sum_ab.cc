888f7073195ba276eecfe7de4ebae214b605785a
diegomarzaa <al426641@uji.es>
2024-01-30 11:27:02 +0100

    sum_ab v4

--- Sum/sum_ab.cc
+++ Sum/sum_ab.cc
@@ -15,14 +15,12 @@ int main() {
 
     std::cout << "Enter b number >= 1: " << std::endl;
     std::cin >> b;
-    while (b<1)
+    while (b<a)
     {
         std::cout << "Enter b number >= 1: " << std::endl;
         std::cin >> b;
     }
     
-
-    std::cin >> b;
 
     for (int num_act = a; num_act<=b; num_act++) {
         sum += num_act;
