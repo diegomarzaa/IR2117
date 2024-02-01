6be08f8acff2a91f60a12347666a51b9fd435f79
diegomarzaa <al426641@uji.es>
2024-02-01 13:03:31 +0100

    quadratic_v1

---
+++ Quadratic/quadratic.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main() 
+{
+  double a, b, c;
+
+  std::cout << "Introduce el coeficiente a: ";
+  std::cin >> a;
+
+  std::cout << "Introduce el coeficiente b: ";
+  std::cin >> b;
+  
+  std::cout << "Introduce el coeficiente c: ";
+  std::cin >> c;
+  
+  double solucion = 0;
+
+  std::cout << "La solución es: " << solucion << std::endl;
+
+  return 0;
+}
