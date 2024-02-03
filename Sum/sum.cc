3cc7cb8a81012669a4983f55221f3d956ce262d0
diegomarzaa <al426641@uji.es>
2024-02-03 13:19:31 +0100

    sum v5 millores

--- Sum/sum.cc
+++ Sum/sum.cc
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <string>
 
-int sumar(int n) {
+int sumar(int n) 
+{
     int sum = 0;
     for (int i = 1; i<=n; i++) {
-    sum += i;
+        sum += i;
     }
     return sum;
 }
 
-int main() {
+int main() 
+{
     int number;
     std::cout << "Enter a number: " << std::endl;
     std::cin >> number;
 
-    while (number < 1)
-    {
+    while (number < 1) {
         std::cout << "You must enter a positive number." << std::endl;
         std::cout << "Enter a number: " << std::endl;
         std::cin >> number;
     }
     
     std::cout << "The sum from 1 to " << number << " is " << sumar(number) << std::endl;
-
     return 0;
 }
