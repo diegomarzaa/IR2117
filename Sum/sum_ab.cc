909d409b27dc1e2e148f26be56d201ab638ff791
diegomarzaa <al426641@uji.es>
2024-02-03 13:17:15 +0100

    sum v5.2
    Millores d'estil

--- Sum/sum_ab.cc
+++ Sum/sum_ab.cc
@@ -1,27 +1,24 @@
 #include <iostream>
-#include <string>
 
-int main() {
+int main() 
+{
     int a, b;
     int sum = 0;
+
     std::cout << "Enter a number >= 1: " << std::endl;
     std::cin >> a;
-    while (a<1)
+    while (a < 1)
     {
         std::cout << "Enter a number >= 1: " << std::endl;
         std::cin >> a;
     }
-    
-
     std::cout << "Enter b number >= 1: " << std::endl;
     std::cin >> b;
-    while (b<a)
-    {
+    while (b < a) {
         std::cout << "Enter b number >= 1: " << std::endl;
         std::cin >> b;
     }
     
-
     for (int num_act = a; num_act<=b; num_act++) {
         sum += num_act;
     }
