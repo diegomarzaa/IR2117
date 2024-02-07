5177e300f3e7696a2720a2f41961257e14f04f12
diegomarzaa <al426641@uji.es>
2024-02-07 19:33:32 +0100

    millores_indentació

--- Sum/sum.cc
+++ Sum/sum.cc
@@ -3,25 +3,25 @@
 
 int sumar(int n) 
 {
-    int sum = 0;
-    for (int i = 1; i<=n; i++) {
-        sum += i;
-    }
-    return sum;
+  int sum = 0;
+  for (int i = 1; i<=n; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 int main() 
 {
-    int number;
+  int number;
+  std::cout << "Enter a number: " << std::endl;
+  std::cin >> number;
+
+  while (number < 1) {
+    std::cout << "You must enter a positive number." << std::endl;
     std::cout << "Enter a number: " << std::endl;
     std::cin >> number;
+  }
 
-    while (number < 1) {
-        std::cout << "You must enter a positive number." << std::endl;
-        std::cout << "Enter a number: " << std::endl;
-        std::cin >> number;
-    }
-    
-    std::cout << "The sum from 1 to " << number << " is " << sumar(number) << std::endl;
-    return 0;
+  std::cout << "The sum from 1 to " << number << " is " << sumar(number) << std::endl;
+  return 0;
 }
--- Sum/sum_ab.cc
+++ Sum/sum_ab.cc
@@ -2,27 +2,27 @@
 
 int main() 
 {
-    int a, b;
-    int sum = 0;
+  int a, b;
+  int sum = 0;
 
+  std::cout << "Enter a number >= 1: " << std::endl;
+  std::cin >> a;
+  while (a < 1)
+  {
     std::cout << "Enter a number >= 1: " << std::endl;
     std::cin >> a;
-    while (a < 1)
-    {
-        std::cout << "Enter a number >= 1: " << std::endl;
-        std::cin >> a;
-    }
+  }
+  std::cout << "Enter b number >= 1: " << std::endl;
+  std::cin >> b;
+  while (b < a) {
     std::cout << "Enter b number >= 1: " << std::endl;
     std::cin >> b;
-    while (b < a) {
-        std::cout << "Enter b number >= 1: " << std::endl;
-        std::cin >> b;
-    }
-    
-    for (int num_act = a; num_act<=b; num_act++) {
-        sum += num_act;
-    }
+  }
+  
+  for (int num_act = a; num_act<=b; num_act++) {
+    sum += num_act;
+  }
 
-    std::cout << "The sum from " << a << " to " << b << " is " << sum << std::endl;
-    return 0;
+  std::cout << "The sum from " << a << " to " << b << " is " << sum << std::endl;
+  return 0;
 }
