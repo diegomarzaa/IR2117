3039bcc9e537f80b9c25fafa69686915f0ea7074
diegomarzaa <al426641@uji.es>
2024-02-14 12:44:19 +0100

    stats_v8

--- ros2_ws/src/median/src/median.cpp
+++ ros2_ws/src/median/src/median.cpp
@@ -11,10 +11,24 @@ void topic_callback(const std_msgs::msg::Int32::SharedPtr msg){
 
   // Sort the numbers
   std::sort(numbers.begin(), numbers.end());
+
+  // Calculate the median
+  int n = numbers.size();   // Number of elements in the vector
+  float median = 0;         // Value to return
+
+  // Número par: Promedio de los dos números centrales
+  if (n % 2 == 0){
+    median = (numbers[n/2 - 1] + numbers[n/2]) / 2;
+    std::cout << "Número par. " << "Promedio entre " << numbers[n/2 - 1] << " y " << numbers[n/2] << " da una mediana de: " << median << std::endl;
+
+  // Número impar: El número central
+  } else {
+    median = numbers[n/2];
+    std::cout << "Número impar. " << "Mediana: " << median << std::endl;
+  }
   
   // Display the numbers in the vector
   std::string numbers_str = "";
-  int n = numbers.size();
   for (int i = 0; i < n; i++){
     numbers_str += std::to_string(numbers[i]);
     if (i < n-1){
@@ -22,7 +36,7 @@ void topic_callback(const std_msgs::msg::Int32::SharedPtr msg){
     }
   }
 
-  std::cout << "List of numbers: " << numbers_str << std::endl;
+  std::cout << "List of numbers: " << numbers_str << std::endl << std::endl;
 }
 
 int main(int argc, char * argv[])
