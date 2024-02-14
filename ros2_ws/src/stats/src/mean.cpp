f29802bcf3c1761485ed998283ada8386a975375
diegomarzaa <al426641@uji.es>
2024-02-14 12:08:17 +0100

    stats_v4_floatArreglos

--- ros2_ws/src/stats/src/mean.cpp
+++ ros2_ws/src/stats/src/mean.cpp
@@ -1,17 +1,18 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
+#include "std_msgs/msg/float32.hpp"   // Import this.
 #include <iostream>
 
 // RUNNING THE NODE
 //1 terminal $ ros2 run stats mean          (nothing happens for now)
-//2 terminal $ ros2 topic pub /number std_msgs/msg/Int32 "{data: '1'}"    (now in the first terminal we see "Mensaje recibido: 1" each second)
+//2 terminal $ ros2 topic pub /number std_msgs/msg/float32 "{data: '1'}"    (now in the first terminal we see "Mensaje recibido: 1" each second)
 //3 terminal $ ros2 topic echo /mean        (data: 1, data: 2, data: 3...)  (this one must be executed last, while the other two are running, if not, it will show WARNING: topic [/mean] does not appear to be published yet)
 
-float sum = 0;
+double sum = 0;
 int message_count = 0;
-float mean_to_publish = 0;
+double mean_to_publish = 0;
 
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>> publisher;
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32>> publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) {
 
@@ -23,11 +24,11 @@ void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) {
   message_count++;
   mean_to_publish = sum / message_count;
 
-  std_msgs::msg::Int32 out_msg;
+  std_msgs::msg::Float32 out_msg;
   out_msg.data = mean_to_publish;
   publisher->publish(out_msg);
 
-  std::cout << "Mensaje recibido: " << msg->data << std::endl;
+  std::cout << "Número recibido: " << msg->data << std::endl;
 }
 
 int main(int argc, char * argv[])
@@ -41,7 +42,7 @@ int main(int argc, char * argv[])
   
   // PUBLICADOR AL TOPIC "/mean"
   // Se publica el valor del contador de mensajes cada vez que se recibe un mensaje.
-  publisher = node->create_publisher<std_msgs::msg::Int32>("mean", 10);
+  publisher = node->create_publisher<std_msgs::msg::Float32>("mean", 10);
 
   rclcpp::spin(node);
   rclcpp::shutdown();
