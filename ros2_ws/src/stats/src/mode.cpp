c223a483b8b1e37f33ef2a834a1b2c0b9e4c3535
diegomarzaa <al426641@uji.es>
2024-02-15 19:44:18 +0100

    stats_v11

--- ros2_ws/src/stats/src/mode.cpp
+++ ros2_ws/src/stats/src/mode.cpp
@@ -1,17 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
-#include "std_msgs/msg/float32.hpp"   // Import this.
 #include <iostream>
 
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>> publisher;   // Publisher to topic "/median"
 std::map<int, int> numbers_counter;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32>> publisher;   // Publisher to topic "/median"
+int number_biggest_occurrence = 0;
+int biggest_occurrence = 0;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) {
   int new_number = msg->data;
   numbers_counter[new_number]++;
 
-  std::cout << "Nuevo número: " << new_number << std::endl;
-  std::cout << "El número 3 ha aparecido " << numbers_counter[3] << " veces." << std::endl;
+  if (numbers_counter[new_number] > biggest_occurrence) {
+    number_biggest_occurrence = new_number;
+    biggest_occurrence = numbers_counter[new_number];
+  }
+
+  std_msgs::msg::Int32 moda_publicar;
+  moda_publicar.data = number_biggest_occurrence;
+  publisher->publish(moda_publicar);
+  std::cout << "Moda: " << number_biggest_occurrence << std::endl;
 }
 
 int main(int argc, char * argv[])
@@ -23,7 +31,7 @@ int main(int argc, char * argv[])
   auto subscription = node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
 
   // PUBLICADOR AL TOPIC "/mode"
-  publisher = node->create_publisher<std_msgs::msg::Float32>("mode", 10);
+  publisher = node->create_publisher<std_msgs::msg::Int32>("mode", 10);
   
   rclcpp::spin(node);
   rclcpp::shutdown();
