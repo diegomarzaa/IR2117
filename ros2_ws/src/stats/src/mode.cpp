a21d128fe26f4407f75186a38c65922d5e805b2f
diegomarzaa <al426641@uji.es>
2024-02-15 20:54:38 +0100

    stats_v12

--- ros2_ws/src/stats/src/mode.cpp
+++ ros2_ws/src/stats/src/mode.cpp
@@ -1,38 +1,39 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
+#include "std_msgs/msg/int32_multi_array.hpp"
+#include <map>
 #include <iostream>
 
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>> publisher;   // Publisher to topic "/median"
-std::map<int, int> numbers_counter;
-int number_biggest_occurrence = 0;
-int biggest_occurrence = 0;
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32MultiArray>> publisher;   // Publisher to topic "/median"
+std::map<int, int> numbers_counter;   // (número: ocurrencias)
+int moda_actual = 0;      // Número máximo de ocurrencias para algun numero
+auto modas = std_msgs::msg::Int32MultiArray();    // Array con las modas que se publicará
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) {
   int new_number = msg->data;
   numbers_counter[new_number]++;
-
-  if (numbers_counter[new_number] > biggest_occurrence) {
-    number_biggest_occurrence = new_number;
-    biggest_occurrence = numbers_counter[new_number];
+  // - Si el número actual tiene más ocurrencias que el número con más ocurrencias:
+  // Sobreescribimos las modas, agregamos la nueva moda mayor
+  // Actualizamos esta info
+  // - Si tiene las mismas ocurrencias que la moda tras actualizar, lo agregamos a las modas
+  if (numbers_counter[new_number] > moda_actual) {
+    modas.data.clear();
+    modas.data.push_back(new_number);
+    moda_actual = numbers_counter[new_number];
+  } else if (numbers_counter[new_number] == moda_actual) {
+    modas.data.push_back(new_number);
   }
-
-  std_msgs::msg::Int32 moda_publicar;
-  moda_publicar.data = number_biggest_occurrence;
-  publisher->publish(moda_publicar);
-  std::cout << "Moda: " << number_biggest_occurrence << std::endl;
+  publisher->publish(modas);
 }
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("mode");
-
   // SUSCRIPTOR AL TOPIC "/number"
   auto subscription = node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-
-  // PUBLICADOR AL TOPIC "/mode"
-  publisher = node->create_publisher<std_msgs::msg::Int32>("mode", 10);
-  
+  // PUBLICADOR AL TOPIC "/mode", un array con todas las modas (enteros)
+  publisher = node->create_publisher<std_msgs::msg::Int32MultiArray>("mode", 10);
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
