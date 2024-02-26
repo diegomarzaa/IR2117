9505c9c30dbb5ac1f43a9a046336c99060df2654
diegomarzaa <al426641@uji.es>
2024-02-26 11:04:10 +0100

    minmax_v4

--- ros2_ws/src/minmax/src/min.cpp
+++ ros2_ws/src/minmax/src/min.cpp
@@ -2,18 +2,21 @@
 #include "std_msgs/msg/int32.hpp"
 #include <iostream>
 
+int min_number;
+
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) {
   int input = msg->data;
-  std::cout << "Received number: " << input << std::endl;
+  if (not min_number or input < min_number) {
+    min_number = input;
+  }
+  std::cout << "Min number:" << min_number << std::endl;
 }
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-
   auto node = rclcpp::Node::make_shared("min");
   auto subscription = node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
