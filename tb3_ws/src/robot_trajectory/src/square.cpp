67411aa45a5bf4807aaeaa49271d61cfbedf3556
diegomarzaa <al426641@uji.es>
2024-03-14 12:37:53 +0100

    ROS parameter square_lenght

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -2,7 +2,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp" // Canviem el tipus de fitxer al de moure motors
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std::chrono_literals;   // Si no es posa aquesta linia, hauriem de posar std::chrono::milliseconds(500) en lloc de 500ms
 
@@ -13,25 +13,26 @@ int main(int argc, char * argv[])     // argc: nombre d'arguments, argv: punter 
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);     // El 10 es el tamany de la cua, se descartaran els primers missatges si la cua esta plena
   node->declare_parameter("linear_speed", 0.1);    // Declarar un paràmetre amb el valor per defecte 0.1
   node->declare_parameter("angular_speed", M_PI / 20);
+  node->declare_parameter("square_length", 1.0);
   geometry_msgs::msg::Twist message;    // Missatge per a moure el robot
   
   // Dades moviment
   rclcpp::WallRate loop_rate(10ms);
   int i=0;
+  double square_length = node->get_parameter("square_length").get_parameter_value().get<double>();
+  double loop_wait = 0.01;
   
   // Dades avant
-  double distance = 1.0;
+  double distance = square_length;
   double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();    // Obtenir el valor del paràmetre
   std::cout << "linear_speed: " << linear_speed << std::endl;
-  double total_time_forward = distance / linear_speed;
-  int n_avant = total_time_forward / 0.01;    // iteracions
+  double linear_iterations = distance / (loop_wait * linear_speed);
 
   // Dades gir
   double angle = 90 * M_PI / 180;           // 90 graus a radians
   double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
   std::cout << "angular_speed: " << angular_speed << std::endl;
-  double total_time_rotation = angle / angular_speed;
-  int n_gir = total_time_rotation / 0.01;
+  double angular_iterations = angle / (loop_wait * angular_speed);
 
   // Moviment quadrat
   for(int j=0; j<4; j++)
@@ -39,7 +40,7 @@ int main(int argc, char * argv[])     // argc: nombre d'arguments, argv: punter 
     i=0;
     
     std::cout << "Avant" << std::endl;
-    while (rclcpp::ok() && i<n_avant) {
+    while (rclcpp::ok() && i<linear_iterations) {
       i++;
       message.linear.x = linear_speed;
       publisher->publish(message);
@@ -52,7 +53,7 @@ int main(int argc, char * argv[])     // argc: nombre d'arguments, argv: punter 
     i=0;
 
     std::cout << "Gir" << std::endl;
-    while (rclcpp::ok() && i<n_gir) {
+    while (rclcpp::ok() && i<angular_iterations) {
       i++;
       message.angular.z = angular_speed;
       publisher->publish(message);
