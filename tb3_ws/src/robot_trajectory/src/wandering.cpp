e1af8c775eaf5bae3a84d699b3443bc0c7044437
diegomarzaa <al426641@uji.es>
2024-04-16 21:02:45 +0200

    wandering v7

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -6,36 +6,27 @@
 using namespace std::chrono_literals;   // Si no es posa aquesta linia, hauriem de posar std::chrono::milliseconds(500) en lloc de 500ms
 rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher;
 
+float min_esquerra;
+float min_dreta;
+
 void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
 
   auto moviment = geometry_msgs::msg::Twist();
-  float min = msg->ranges[0];
+  min_dreta = msg->ranges[0];
   for (int i = 0; i <= 9; ++i) {
-    if (msg->ranges[i] < min) {
-      min = msg->ranges[i];
+    if (msg->ranges[i] < min_dreta) {
+      min_dreta = msg->ranges[i];
     }
   }
+  std::cout << "El valor mínim a la dreta és: " << min_dreta << std::endl;
+
+  min_esquerra = msg->ranges[350];
   for (int i = 350; i <= 359; ++i) {
-    if (msg->ranges[i] < min) {
-      min = msg->ranges[i];
+    if (msg->ranges[i] < min_esquerra) {
+      min_esquerra = msg->ranges[i];
     }
   }
-
-  std::cout << "El valor mínim és: " << min << std::endl;
-  
-  if (min > 0.5) {
-    std::cout << "No hi ha cap obstacle a menys de 0.5m" << std::endl;
-    moviment.linear.x = 0.5;
-    moviment.angular.z = 0.0;
-    publisher->publish(moviment);
-
-  } else {
-    std::cout << "Hi ha un obstacle a menys de 0.5m, girant a la esquerra" << std::endl;
-    moviment.linear.x = 0.0;
-    moviment.angular.z = 0.5;
-    publisher->publish(moviment);
-  }
-
+  std::cout << "El valor mínim a l'esquerra és: " << min_esquerra << std::endl;
 }
 
 int main(int argc, char * argv[])     // argc: nombre d'arguments, argv: punter a un array de punter a caràcters
@@ -49,8 +40,29 @@ int main(int argc, char * argv[])     // argc: nombre d'arguments, argv: punter 
   rclcpp::WallRate loop_rate(10ms);    // Frecuencia per a que el bucle es repetisca (usa chrono)
 
   while (rclcpp::ok()) {    // Bucle principal del programa
+    auto moviment = geometry_msgs::msg::Twist();
+    
+    if (min_esquerra < 0.5 or min_dreta < 0.5) {
+      std::cout << "Hi ha un obstacle a menys de 0.5m." << std::endl;
+      if (min_esquerra > min_dreta) {
+        std::cout << "Girant a la esquerra" << std::endl;
+        moviment.linear.x = 0.0;
+        moviment.angular.z = -0.5;
+      } else {
+        std::cout << "Girant a la dreta" << std::endl;
+        moviment.linear.x = 0.0;
+        moviment.angular.z = 0.5;
+      }
+    } else {
+      std::cout << "No hi ha cap obstacle a menys de 0.5m." << std::endl;
+      moviment.linear.x = 0.5;
+      moviment.angular.z = 0.0;
+    }
+    
+    publisher->publish(moviment);
     rclcpp::spin_some(node);
     loop_rate.sleep();
+
   }
 
   rclcpp::shutdown();   // Finalitzar el ROS
