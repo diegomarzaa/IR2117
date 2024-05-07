798b5091abbc543635bb88ebbc6281842b20ef79
diegomarzaa <al426641@uji.es>
2024-05-07 20:18:18 +0200

    olympic v5

--- ros2_ws/src/olympic/src/rings.cpp
+++ ros2_ws/src/olympic/src/rings.cpp
@@ -8,14 +8,20 @@ int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("rings");
+  node->declare_parameter("radius", 1.0);
+
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
   geometry_msgs::msg::Twist message;
   auto publish_count = 0;
   rclcpp::WallRate loop_rate(500ms);
 
   while (rclcpp::ok()) {
-    message.linear.x = 1.0;
-    message.angular.z = 1.0;
+    double radius;
+    double linear_vel = 2.0;
+    node->get_parameter("radius", radius);
+
+    message.linear.x = linear_vel;
+    message.angular.z = linear_vel / radius;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
